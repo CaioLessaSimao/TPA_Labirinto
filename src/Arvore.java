f33303540a915e6039d2028e017101052d5213ff
Caio Lessa <caiolessa02@gmail.com>
2024-10-07 22:22:56 -0300

    Tudo errado mas é um começo

---
+++ src/Arvore.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Arvore {
+    Node raiz;
+
+    public Arvore(){
+        this.raiz = null;
+    }
+
+    public void geraArvore(int[][] matriz){
+        int raiz = new Node(matriz[0][0]);
+    }
+}
---
+++ src/Node.java
@@ -0,0 +1,10 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Node {
+    Node pai;
+    ArrayList<Node> filhos;
+    Ponto posicao;
+
+    public Node (Ponto p){}
+}
